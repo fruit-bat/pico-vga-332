78f57f96121e57850c4cc70442cec73850143b73
Phil <pmscull@gmail.com>
2022-05-18 22:52:08 +0100

    Alt key map

--- pzx_keyscan.cpp
+++ pzx_keyscan.cpp
@@ -30,6 +30,22 @@ static uint8_t kbits[6][6] = {
   { HID_KEY_Q, HID_KEY_W, HID_KEY_E, HID_KEY_R, HID_KEY_T, HID_KEY_ALT_LEFT }
 };
 
+static uint8_t akbits[6][6] = {
+  // Row 0
+  { HID_KEY_SPACE, HID_KEY_COMMA, HID_KEY_M, HID_KEY_N, HID_KEY_B, HID_KEY_ARROW_DOWN },
+  // Row 1
+  { HID_KEY_ENTER, HID_KEY_L, HID_KEY_K, HID_KEY_J, HID_KEY_H, HID_KEY_ARROW_LEFT },
+  // Row 2
+  { HID_KEY_0, HID_KEY_9, HID_KEY_8, HID_KEY_7, HID_KEY_6, HID_KEY_ARROW_UP },
+  // Row 3
+  { HID_KEY_EQUAL, HID_KEY_Z, HID_KEY_X, HID_KEY_C, HID_KEY_V, HID_KEY_ARROW_RIGHT },
+  // Row 4
+  { HID_KEY_A, HID_KEY_S, HID_KEY_D, HID_KEY_F, HID_KEY_G, HID_KEY_ESCAPE },
+  // Row 5
+  { HID_KEY_1, HID_KEY_2, HID_KEY_3, HID_KEY_4, HID_KEY_5, HID_KEY_ALT_LEFT }
+};
+
+
 void pzx_keyscan_init() {
   
     for(int i = 0; i < 6; ++i) {
